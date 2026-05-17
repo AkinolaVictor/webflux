49624576a11124e772c30fdfaa55f549ffc1f0f3
Boughous-startup <akinolavictor50@gmail.com>
2026-05-17 23:31:34 +0100

    first commit

---
+++ src/lib/utils/deta.ts
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function Deta(props: Props) {
+    const {} = props
+
+    return (
+        <div>
+            Deta
+        </div>
+    )
+}
+
+export default Deta
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,27 @@
 import MainComp from "@/components/mainComp";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export default function Home() {
+  // jsrepo init --registry
   return (
     <div
       className={`w-full h-auto `}
       // className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
-      <MainComp />
+      {/* <MainComp /> */}
+      <div>
+        Webflux Page and registry
+      </div>
     </div>
   );
 }
