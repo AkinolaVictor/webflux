c5090697cd59c5246d5c2a7e5be7cef29c6628f9
Boughous-startup <akinolavictor26@gmail.com>
2026-07-02 01:23:31 +0100

    'completed_text_Fadings'

--- src/utils/comp_dir/transitions.js
+++ src/utils/comp_dir/transitions.js
@@ -4,20 +4,20 @@ export const transitions = {
     section: "Transitions",
     Icon: ()=><Workflow size={"13px"}/>,
     content: [
-        {
-            title: "Sidebar",
-            href: "/components/coqsmponents",
-            id: "mainb"
-        },
-        {
-            title: "DropDown Menu",
-            href: "/components/cocmponents",
-            id: "mainn"
-        },
-        {
-            title: "Text By Height",
-            href: "/components/caomponents",
-            id: "mainnw"
-        },
+        // {
+        //     title: "Sidebar",
+        //     href: "/components/coqsmponents",
+        //     id: "mainb"
+        // },
+        // {
+        //     title: "DropDown Menu",
+        //     href: "/components/cocmponents",
+        //     id: "mainn"
+        // },
+        // {
+        //     title: "Text By Height",
+        //     href: "/components/caomponents",
+        //     id: "mainnw"
+        // },
     ]
 }
